e36f1ba1b5a1656f0a1b7f8ae7d34fac8d6a6780
Mohamad Mohamed <mohamad_mohamed@hotmail.de>
2022-04-10 13:48:52 +0200

    Initial commit

---
+++ 04_lecture_SQL/airline/flights/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+    
+## First example    
+# class Flight(models.Model):
+#     origin = models.CharField(max_length=64)
+#     destination = models.CharField(max_length=64)
+#     duration = models.IntegerField()
+    
+#     def __str__(self):
+#         return f"{self.id}: {self.origin} to {self.destination}"
+
+#Example with foreign key
+class Flight(models.Model):
+    #related name accessing a relationship in reverse order
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+    
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+    
+    def __str__(self):
+        return f"{self.first} {self.last}"
